74e283594b05ca9561db21db17f96cd9ebe0a4f1
soputan12 <soputanbryant@gmail.com>
2023-04-18 17:23:35 +0800

    removed unnecessary comments

--- check.py
+++ check.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 """ TODO: change subject checker to class in future """
-def check_prerequisites(current_subject, taken_subject_list): # function to check prerequisites, only checking for CP1401 atm
+def check_prerequisites(current_subject, taken_subject_list): # function to check prerequisites
     taken_subjects = str(taken_subject_list) # made life a bit easier by making this into a var
     credit_point = len(taken_subject_list) * 3 # calculate credit points: 1 subject = 3 credit points
     if current_subject == "CP1404":
@@ -11,10 +11,10 @@ def check_prerequisites(current_subject, taken_subject_list): # function to chec
         else:
             return False
     elif current_subject == "MA1000":
-        if "MA1020" in taken_subjects:
+        if "MA1020" in taken_subjects: # dont need to take if got MA1020 inside
             return False
     elif current_subject == "MA1020":
-        if "MA1000" in taken_subjects:
+        if "MA1000" in taken_subjects: # dont need to take if got MA1000 inside
             return False
     elif current_subject == "MA1580":
         if "MA1000" or "MA1020" in taken_subjects:
@@ -156,9 +156,9 @@ def get_sp51_23_subjects(subject_counter, row_counter, subject_list, sp51_23_lis
                 row_counter += 1 # if 0 skip to next row
     return current_sp_subjects
 
-def sp52_availability(df): # creates a list for sp51 2023 availability
-    sp52_23_availability = df.loc[:, "sp52_23"] #pandas magic, returns the sp51_23 row
-    sp52_23_list = [] # list to add the sp51_23 stuff into a list
+def sp52_availability(df): 
+    sp52_23_availability = df.loc[:, "sp52_23"] 
+    sp52_23_list = [] 
     for i in sp52_23_availability: # loop to add all the stuff into list
         if i == 1:
             sp52_23_list.append(1)
@@ -166,7 +166,7 @@ def sp52_availability(df): # creates a list for sp51 2023 availability
             sp52_23_list.append(0)
     return sp52_23_list
 
-def get_sp52_23_subjects(subject_counter, row_counter, subject_list, sp52_23_list, taken_subject_list, current_sp_subjects): # function to get sp51 2023 subjects
+def get_sp52_23_subjects(subject_counter, row_counter, subject_list, sp52_23_list, taken_subject_list, current_sp_subjects): 
     current_sp_subjects = []
     while subject_counter < 4: # end condition for loop
         current_subject = str(subject_list[row_counter])
@@ -191,9 +191,9 @@ def get_sp52_23_subjects(subject_counter, row_counter, subject_list, sp52_23_lis
                 row_counter += 1 # if 0 skip to next row
     return current_sp_subjects
 
-def sp53_availability(df): # creates a list for sp51 2023 availability
-    sp53_23_availability = df.loc[:, "sp52_23"] #pandas magic, returns the sp51_23 row
-    sp53_23_list = [] # list to add the sp51_23 stuff into a list
+def sp53_availability(df): 
+    sp53_23_availability = df.loc[:, "sp52_23"] 
+    sp53_23_list = [] 
     for i in sp53_23_availability: # loop to add all the stuff into list
         if i == 1:
             sp53_23_list.append(1)
@@ -201,7 +201,7 @@ def sp53_availability(df): # creates a list for sp51 2023 availability
             sp53_23_list.append(0)
     return sp53_23_list     
 
-def get_sp53_23_subjects(subject_counter, row_counter, subject_list, sp53_23_list, taken_subject_list, current_sp_subjects): # function to get sp51 2023 subjects
+def get_sp53_23_subjects(subject_counter, row_counter, subject_list, sp53_23_list, taken_subject_list, current_sp_subjects): 
     current_sp_subjects = []
     while subject_counter < 4: # end condition for loop
         current_subject = str(subject_list[row_counter])
@@ -231,16 +231,8 @@ def main():
     df = pd.DataFrame(data, columns=['subject_code', 'prerequisites', 'sp51_23', 'sp52_23', 'sp53_23', 'sp51_24', 'sp52_24', 'sp53_24'])
     subject_counter = 0 # how many subjects you're taking for 1 study period
     taken_subject_list = [] # subjects taken already
-    study_period = 2 # which study period
-    credit_point = 0
     row_counter = 0
     current_sp_subjects = [] # subjects your taking in this sp
-    #if counter  [10]: SPECIAL CHECK FOR CREDIT POINT ROWS 
-        #if row[1] in taken_subject_list and credit_point >= 12:
-
-    # index = ['CP1401', 'CP1402', 'CP1404', 'CP1406', 'MA1000', 'MA1020', 'MA1580', 'CP1409', 'CP1410', 'CP2403', 'CP2404', 'CP2406', 'CP2409', 'CP2421', 'CP2422', 'CP2423', 'CP2424', 'CP3403', 'CP3405', 'CP3406', 'CP3407', 'CP3414', 'CP3415', 'CP3416', 'CP3417', 'CP3418']
-    # df.index = index
-    # print(df)
 
     sp51_23_list = sp51_availability(df)
     subject_list = create_subject_list(df)
