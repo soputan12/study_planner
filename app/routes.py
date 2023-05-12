c871278976c3a7f91d9ab96e580f539c1c03f39e
soputan12 <soputanbryant@gmail.com>
2023-05-12 20:36:44 +0800

    created basic checkbox functionality

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print(request.form.getlist('mycheckbox'))
+        return 'DONE'
     return render_template('index.html', title='Home')
 
 @app.route('/results')
 def results():
-    return render_template('results.html', title='Home')
+    return render_template('results.html', title='Results')
